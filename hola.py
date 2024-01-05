c12026e41fc6e2a1ae344281914a869e71fa56ce
Daniela Zuñiga <nta.danaz@gmail.com>
2024-01-05 12:04:39 -0300

    guadado 1

--- hola.py
+++
@@ -1,1 +0,0 @@
-print("hola creo que cree algo")
---
+++ suma.py
@@ -0,0 +1,7 @@
+def suma(a, b):
+    return a + b
+    
+numero3 = float(input("Ingrese un número: "))
+numero4 = float(input("Ingrese otro número: "))
+resultado_usuario = suma(numero3, numero4)
+print("La suma de los números ingresados es:", resultado_usuario)
