dbea0a656fc5a2c9695b842e11536c4c70ca4640
cxrlosarmando <carlos.armandor28@gmail.com>
2024-01-05 10:42:36 -0300

    creacion de suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+print("Bienvenidos/as a su calculadora de sumas de Python cohorte 10")
+
+numero1 = int(input("Ingresa tu primer numero para la suma: "))
+numero2 = int(input("Ingresa tu segundo numero : "))
+resultado = (numero1 + numero2)
+
+print("El resultado de la suma es: ", resultado) 
