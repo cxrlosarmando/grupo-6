c2fd5c4e6da66f67e9d020949a5d54fa6354ee8e
Daniela Zuñiga <nta.danaz@gmail.com>
2024-01-05 12:26:04 -0300

    cambios sumaaaaaaa

--- suma.py
+++ suma.py
@@ -5,3 +5,5 @@ numero3 = float(input("Ingrese un número: "))
 numero4 = float(input("Ingrese otro número: "))
 resultado_usuario = suma(numero3, numero4)
 print("La suma de los números ingresados es:", resultado_usuario)
+
+#hola cambios
