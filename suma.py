ba49ed868994b418244eb99d798a81d6c83c1cf0
cxrlosarmando <carlos.armandor28@gmail.com>
2024-01-05 13:23:17 -0300

    comentario en suma de dani

--- suma.py
+++ suma.py
@@ -5,3 +5,4 @@ numero3 = float(input("Ingrese un número: "))
 numero4 = float(input("Ingrese otro número: "))
 resultado_usuario = suma(numero3, numero4)
 print("La suma de los números ingresados es:", resultado_usuario)
+#Lindo quedó el codigo!!!!
