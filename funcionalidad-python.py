ebb3335def3205dcdc229472a88b2cc3d6dc69ab
LilDie <neobad.reyes@gmail.com>
2024-01-05 11:10:57 -0300

    ramadiegoMulti

---
+++ funcionalidad-python.py
@@ -0,0 +1,11 @@
+def multiplicar():
+    try:
+        numero1 = float(input("Ingresa el primer número: "))
+        numero2 = float(input("Ingresa el segundo número: "))
+        resultado = numero1 * numero2
+        print(f"El resultado de la multiplicación de {numero1} y {numero2} es: {resultado}")
+    except ValueError:
+        print("Por favor, ingresa números válidos.")
+
+# Llamada a la función para multiplicar
+multiplicar()
